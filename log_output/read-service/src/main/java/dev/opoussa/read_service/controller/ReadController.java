9cb726e097e510ba75d11143bb1de6b0f8d1ba54
opoussa <poussa.otso@gmail.com>
2026-03-29 21:11:42 +0300

    Implement html template for read service

--- log_output/read-service/src/main/java/dev/opoussa/read_service/controller/ReadController.java
+++ log_output/read-service/src/main/java/dev/opoussa/read_service/controller/ReadController.java
@@ -3,19 +3,25 @@ package dev.opoussa.read_service.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.opoussa.read_service.service.ReadService;
 
-@RestController("/")
+@Controller
 public class ReadController {
     
     @Autowired
     ReadService readService;
 
     @GetMapping
-    public String getCurrentLog() throws IOException {
-        return readService.readCurrentHash();
+    public String getCurrentLog(Model model) throws IOException {
+        var hash = readService.readCurrentHash();
+        var pingPongs = readService.readPings();
+        model.addAttribute("hash", hash);
+        model.addAttribute("pongCount", pingPongs);
+        return "index";
     }
 }
