74bf947f050a2e34c44953d063f10136a5a39452
opoussa <poussa.otso@gmail.com>
2026-04-06 10:47:37 +0300

    Re-indent timeutil

--- todo_app/src/main/java/dev/opoussa/todo_app/util/TimeUtil.java
+++ todo_app/src/main/java/dev/opoussa/todo_app/util/TimeUtil.java
@@ -1,9 +1,11 @@
 package dev.opoussa.todo_app.util;
 
 public class TimeUtil {
-    public static boolean minutesHavePassed(Long startTime, int minutes) {
+    public static boolean minutesHavePassed(
+        Long startTime, int minutes
+    ) {
         var now = System.currentTimeMillis();
         var elapsedTime = now - startTime;
-        return elapsedTime >= minutes * 60 * 1000;
+        return elapsedTime >= (minutes * 60 * 1000);
     }
 }
