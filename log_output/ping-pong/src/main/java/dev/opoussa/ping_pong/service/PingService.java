246ca17b7a776d2f957145b8c088376d440427e6
opoussa <poussa.otso@gmail.com>
2026-04-11 22:22:01 +0300

    PINGPONG: switch back to in-memory pingpong count

--- log_output/ping-pong/src/main/java/dev/opoussa/ping_pong/service/PingService.java
+++ log_output/ping-pong/src/main/java/dev/opoussa/ping_pong/service/PingService.java
@@ -1,52 +1,17 @@
 package dev.opoussa.ping_pong.service;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 @Service
 public class PingService {
-    @Value("${shared.folder.path}")
-    private String sharedPath;
 
-    private static final String FILE_NAME = "pings.txt";
-
+    private static int pingPongCount = 0;
+    
     public void logPingPongCount() {
-        try  {
-            Path file = Path.of(sharedPath, FILE_NAME);
-            System.out.println("Writing to file: " + file);
-
-            Files.createDirectories(file.getParent());
-
-            int currentCount = 0;
-            if (Files.exists(file)) {
-                String content = Files.readString(file);
-                currentCount = content.isBlank() ? 0 : Integer.parseInt(content.trim());
-            }
-
-            currentCount++;
-            String line = String.valueOf(currentCount);
-
-            Files.writeString(
-                file,
-                line,
-                StandardOpenOption.CREATE,
-                StandardOpenOption.TRUNCATE_EXISTING
-            );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        pingPongCount++;
+        System.out.println("Ping pong count increased. Current count: " + pingPongCount);
     }
 
-    public String getPingPongCount() throws IOException {
-        Path file = Path.of(sharedPath + FILE_NAME);
-        if (Files.exists(file)) {
-            String content = Files.readString(file);
-            return content.trim();
-        }
-        throw new IOException("File not found: " + sharedPath);
+    public String getPingPongCount() {
+       System.out.println("Ping pong count requested.");
+       return String.valueOf(pingPongCount);
     }
 }
