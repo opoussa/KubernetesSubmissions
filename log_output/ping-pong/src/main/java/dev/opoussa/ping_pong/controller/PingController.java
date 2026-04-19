b2c30c79d12a27132d0d38e8822ea5154f13f8df
opoussa <poussa.otso@gmail.com>
2026-04-19 22:03:26 +0300

    Remove unused import

--- log_output/ping-pong/src/main/java/dev/opoussa/ping_pong/controller/PingController.java
+++ log_output/ping-pong/src/main/java/dev/opoussa/ping_pong/controller/PingController.java
@@ -3,7 +3,6 @@ package dev.opoussa.ping_pong.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import dev.opoussa.ping_pong.service.PingService;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class PingController {
