272482a0b7c2904655d736e3314fd91d7409a1a8
opoussa <poussa.otso@gmail.com>
2026-09-06 12:34:02 +0300

    Improve initial schema to avoid conflicts on pod restart.

--- log_output/ping-pong/src/main/resources/schema.sql
+++ log_output/ping-pong/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE ping_pong_count (
+CREATE TABLE IF NOT EXISTS ping_pong_count (
     singleton BOOLEAN PRIMARY KEY DEFAULT TRUE CHECK (singleton),
     value INTEGER NOT NULL
 );
 
-INSERT INTO ping_pong_count (value) VALUES (0);
+INSERT INTO ping_pong_count (value) VALUES (0) ON CONFLICT (singleton) DO NOTHING;
